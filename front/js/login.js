f3c670b2e527fe3e8e291afe1ac81bd48aa82413
XimenaPulido <20233tn125@utez.edu.mx>
2025-01-26 22:43:47 -0600

    second commit

--- front/js/login.js
+++ front/js/login.js
@@ -1,17 +1,13 @@
-
 const loginForm = document.getElementById("loginForm");
 const errorMessage = document.getElementById("errorMessage");
-
 
 loginForm.addEventListener("submit", async (event) => {
   event.preventDefault(); 
 
-  
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   try {
-    
     const response = await fetch("http://localhost:8080/auth/login", {
       method: "POST",
       headers: {
@@ -24,13 +20,10 @@ loginForm.addEventListener("submit", async (event) => {
       const data = await response.json();
       const token = data.token;
 
-      
       localStorage.setItem("jwt", token);
 
-      
       window.location.href = "/dashboard.html";
     } else {
-     
       errorMessage.textContent =
         "Credenciales inválidas. Inténtalo nuevamente.";
     }
@@ -39,3 +32,18 @@ loginForm.addEventListener("submit", async (event) => {
     errorMessage.textContent = "Ocurrió un error. Inténtalo más tarde.";
   }
 });
+
+function togglePasswordVisibility() {
+  const passwordInput = document.getElementById("password");
+  const toggleIcon = document.querySelector(".toggle-password i");  
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    toggleIcon.classList.remove("bi-eye");      
+    toggleIcon.classList.add("bi-eye-slash");  
+  } else {
+    passwordInput.type = "password";
+    toggleIcon.classList.remove("bi-eye-slash"); 
+    toggleIcon.classList.add("bi-eye");          
+  }
+}
